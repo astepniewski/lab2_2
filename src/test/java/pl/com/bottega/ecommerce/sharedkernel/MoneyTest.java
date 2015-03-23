060f46b2036536a4b74b3c884132795152111499
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-03-23 16:35:28 +0100

    Dodanie testu metody equals klasy Money dla przypadku true

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -1,14 +1,17 @@
 package pl.com.bottega.ecommerce.sharedkernel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_equals_equalsObjects_sholdBeTrue() {
+		Money m1 = new Money(10.00, "USD");
+		Money m2 = new Money(10.00, "USD");
+
+		assertThat(m1, is(equalTo(m2)));
 	}
 
 }
